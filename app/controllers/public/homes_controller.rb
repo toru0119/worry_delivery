4acc67ca8a620eacd4dbb30aba3439c8a53a350d
toru0119 <s.toru.1513@gmail.com>
2022-01-28 09:15:48 +0000

    Img Install

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,4 @@
 class Public::HomesController < ApplicationController
-  
 
   def top
     @rank_order_details = OrderDetail.find(OrderDetail.group(:item_id).order('count(amount) desc').limit(4).pluck(:id))
