c9ffa1ff9433ac9ba9518c3a6d449df9ed920280
toru0119 <s.toru.1513@gmail.com>
2022-01-29 06:38:24 +0000

    Devise Change

--- app/controllers/member/registrations_controller.rb
+++ app/controllers/member/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Member::RegistrationsController < Devise::RegistrationsController
   # before_action :authenticate_member!,except: [:top, :about]
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
